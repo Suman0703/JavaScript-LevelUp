3c6a0c79c0c8b4105d51f0c2eef0a993728fbd2c
Suman0703 <sumandevimehmi36@gamil.com>
2025-11-30 19:47:20 +0530

    Working On JavaScript

---
+++ Dailog_Boxes/index.js
@@ -0,0 +1,18 @@
+//alert
+alert("Hello! This is an alert message.");
+
+//Confirm
+
+let choice = confirm("Do you want to Continue?");
+
+if(choice){
+    alert("You Clicked Ok!");
+}else{
+     alert("You Clicked Cancel!");
+}
+
+//prompt 
+
+let name = prompt("What is your name?");
+
+alert("Hello"+name+"!");
