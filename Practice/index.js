b4165a1fd5b3e4bb5db06addbbd1a41247cb15d3
Suman0703 <sumandevimehmi36@gamil.com>
2025-11-30 17:53:15 +0530

    Working on JS

--- Practice/index.js
+++ Practice/index.js
@@ -1,1 +1,23 @@
-document.write("Hello JavaScript!");
+document.write("Hello JavaScript! <br>");
+
+//local variable 
+function sum(){
+    let x = 11;
+    let y = 12;
+    let sum = x+y;
+    document.write(sum +"<br>");
+}
+
+sum();
+
+//Global Variable
+
+ let a = 21;
+ let b = 14;
+
+ function s() {
+    let total =  a+b;
+    document.write(total);
+ }
+
+s();
